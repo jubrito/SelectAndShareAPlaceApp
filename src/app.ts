0d6b22ca62bbee6c6372350dd099fe56ec12d3b5
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-01 18:39:15 -0300

    setts up a google api key

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyCXpyHNPV8iLjxJrGk_7SxiqKUIXFC3z7E';
+const mapsApiURL = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${GOOGLE_API_KEY}`
+
 function searchAddressHandler(event: Event){
     event.preventDefault();
     const enteredAddress = addressInput.value;
