cfad65239e31ed3946fdc281285db2e1b263bac1
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-01 19:24:35 -0300

    renders a map with google maps using types

--- src/app.ts
+++ src/app.ts
@@ -4,21 +4,49 @@ const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyCXpyHNPV8iLjxJrGk_7SxiqKUIXFC3z7E";
-function searchAddressHandler(event: Event) {
-  event.preventDefault();
-  const enteredAddress = addressInput.value;
 
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-        enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
-    )
-    .then(response => {
-        console.log(response)
-    })
-    .catch((err) => {
-      console.log("Error:" + err);
-    });
+type GoogleGeocodingResponse = {
+    results: {
+        geometry: { location: { lat: number; lng: number } };
+    }[];
+    status: "OK" | "ZERO_RESULTS";
+};
+
+async function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    
+    // Request needed libraries.
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps");
+    //@ts-ignore
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    axios
+    .get<GoogleGeocodingResponse>(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+            enteredAddress
+            )}&key=${GOOGLE_API_KEY}`
+            )
+            .then((response) => {
+            if (response.data.status != "OK") {
+                throw new Error("Could not fetch location");
+            }
+            const coordinates = response.data.results[0].geometry.location;
+            const map = new Map(document.getElementById("map") as HTMLElement, {
+                center: coordinates,
+                zoom: 16,
+                mapId: 'app'
+            });
+            new AdvancedMarkerElement({
+                map: map,
+                position: coordinates,
+                title: "Uluru",
+            });
+            console.log(response);
+        })
+        .catch((err) => {
+            alert(err.message);
+            console.log("Error:" + err);
+        });
 }
 form.addEventListener("submit", searchAddressHandler);
